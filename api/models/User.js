fc217b10b8a654df1f892f631bdd09c351000703
Daniel gonzález Espinoza <danielglezespinoza@gmail.com>
2017-07-31 17:53:57 -0500

    Refactored code to resemble a real sails hook.

--- api/models/User.js
+++ api/models/User.js
@@ -1,34 +1,25 @@
-/**
-* User.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
-
 module.exports = {
   attributes: {
-  	name: {
-  		type: Sequelize.STRING
-  	},
+    name: {
+      type: Sequelize.STRING,
+    },
     age: {
-    	type: Sequelize.INTEGER
-    }    
+      type: Sequelize.INTEGER,
+    },
   },
-  associations: function () {
-  	User.hasMany(Image, {as: 'images', foreignKey: 'owner'});
+  associations: function() {
+    User.hasMany(Image, {as: "images", foreignKey: "owner",});
   },
   defaultScope: function() {
     return {
-      include: [
-        {model: Image, as: 'images'}
-      ]
-    }
+      include: [{model: Image, as: "images",},],
+    };
   },
   options: {
     freezeTableName: false,
-    tableName: 'user',
+    tableName: "user",
     classMethods: {},
     instanceMethods: {},
-    hooks: {}
-  }
+    hooks: {},
+  },
 };
