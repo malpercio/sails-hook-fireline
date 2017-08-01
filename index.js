6dbada4afe8d85dbc37113950c9c0bf80c457a2f
Daniel gonzález Espinoza <danielglezespinoza@gmail.com>
2017-07-31 20:32:42 -0500

    Refactored code for better scalability

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const initialize = require("./lib/initialize");
 const configure = require("./lib/configure");
-const setAssociation = require("./lib/setAssociation");
-const setDefaultScope = require("./lib/setDefaultScope");
 
 module.exports = (sails) => {
   return {
     configure: configure(sails),
     initialize: initialize(sails),
-    setAssociation: setAssociation(sails),
-    setDefaultScope: setDefaultScope(sails),
   };
 };
